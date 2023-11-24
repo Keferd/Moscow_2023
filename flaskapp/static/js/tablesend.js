beb94e555a0afadffef209559acb5bd87246acd8
Keferd <81792991+Keferd@users.noreply.github.com>
2023-11-24 22:38:20 +0500

    настройка width

--- flaskapp/static/js/tablesend.js
+++ flaskapp/static/js/tablesend.js
@@ -60,21 +60,22 @@ sendfilebtn.addEventListener("click", function (e) {
 
                 var table = new Tabulator("#main__area", {
                     data:tabledata,           //load row data from array
-                    layout: "fitColumns",     //fit columns to width of table
-                    responsiveLayout:"hide",  //hide columns that don't fit on the table
+                    layout: "fitDataFill",
+                    // layout: "fitColumns",     //fit columns to width of table
+                    responsiveLayout:"false",  //hide columns that don't fit on the table
                     addRowPos:"top",          //when adding a new row, add it to the top of the table
                     history:true,             //allow undo and redo actions on the table
                     pagination:"local",       //paginate the data
-                    paginationSize:10,         //allow 7 rows per page of data
+                    paginationSize:20,         //allow 7 rows per page of data
                     paginationCounter:"rows", //display count of paginated rows in footer
-                    movableColumns:true,      //allow column order to be changed
-                    resizableRows: true, // Разрешить изменение высоты строк
-                    wordWrap: true, // Разрешить перенос текста в ячейках
+                    // movableColumns:false,      //allow column order to be changed
+                    // resizableRows: true, // Разрешить изменение высоты строк
+                    // wordWrap: false, // Разрешить перенос текста в ячейках
                     columns:[                 //define the table columns
-                        {title:"Исполнитель", field:"executor", sorter:"string",  editor:true},
-                        {title:"Группа тем", field:"group", sorter:"string",editor:true},
-                        {title:"Текст инцидента", field:"text", formatter:"textarea", editor:true},
-                        {title:"Тема", field:"subject", sorter:"string", editor:true},
+                        {title:"Исполнитель", field:"executor", width: "10%", formatter:"textarea",  editor:true},
+                        {title:"Группа тем", field:"group", width: "10%", formatter:"textarea", editor:true},
+                        {title:"Текст инцидента", field:"text", width: "60%", formatter:"textarea", editor:true},
+                        {title:"Тема", field:"subject", width: "20%", formatter:"textarea", editor:true},
                     ],
                 });
             
@@ -95,118 +96,4 @@ sendfilebtn.addEventListener("click", function (e) {
     else {
         
     }
-
-
-    // let text = document.getElementById("main__textarea").value;
-    
-    // let formdata = new FormData();
-    // formdata.append('text', text);
-
-    // if (typeof text != 'undefined') {
-    //     document.getElementById("main__area").innerHTML = `
-    //         <div class="img__container">
-    //             <img class="img__loading" src="static/img/loading.png" alt="loading">
-    //         </div>
-
-    //         <style>
-    //             .img__container {
-    //                 flex: 1;
-    //                 width: 100%;
-    //                 height: 100%;
-    //                 display: flex;
-    //                 align-items: center;
-    //                 justify-content: center;
-    //             }
-
-    //             .img__loading {
-    //                 width: 100px;
-    //                 height: 100px;
-    //                 animation: rotate_img 0.5s linear infinite;
-    //             }
-
-    //             @keyframes rotate_img {
-    //                 0% {
-    //                   transform: rotate(0deg);
-    //                 }
-    //                 100% {
-    //                   transform: rotate(360deg);
-    //                 }
-    //               }
-    //         </style>
-    //     `;
-        
-
-    //     fetch("/api/text",
-    //     {
-    //         method: "POST",
-    //         body: formdata,
-    //         /*headers: {
-    //             'Content-Type': 'multipart/form-data'
-    //         }*/
-    //     })
-    //     .then( response => {
-    //         response.json().then(function(data) {
-
-    //             var encodedImage = data.image_url;
-    //             var decodedImage = atob(encodedImage);
-    //             var byteCharacters = decodedImage.split('').map(char => char.charCodeAt(0));
-    //             var byteArray = new Uint8Array(byteCharacters);
-    //             var imageType = 'image/jpeg';
-    //             var blob = new Blob([byteArray], { type: imageType });
-    //             var imageUrl = URL.createObjectURL(blob);
-
-
-    //             var executor = data.executor;
-    //             var group = data.group;
-    //             var subject = data.subject;
-
-
-    //             document.getElementById("main__result-area").innerHTML = `
-    //                 <p class="result-text result-text_title">Исполнитель:</p>
-    //                 <p class="result-text">` + executor + `</p>
-    //                 <p class="result-text result-text_title">Группа тем:</p>
-    //                 <p class="result-text">` + group + `</p>
-    //                 <p class="result-text result-text_title">Тема:</p>
-    //                 <p class="result-text">` + subject + `</p>
-    //                 <div class="result-partition"></div>
-                    
-    //                 <p class="result-text result-text_title">Облако тэгов:</p>
-    //                 <img class="result-img" src=` + imageUrl + ` alt="Изображение">
-
-    //                 <style>
-    //                 .result-text {
-    //                     color: #ddd;
-    //                     font-size: 16px;
-    //                 }
-
-    //                 .result-text_title {
-    //                     color: #eee;
-    //                     font-weight: bold;
-    //                     margin-top: 20px;
-    //                 }
-
-    //                 .result-partition {
-    //                     width: 100%;
-    //                     border-bottom: 1px solid #eee;
-    //                     margin: 10px 0 30px 0;
-    //                 }
-
-    //                 .result-img {
-    //                     width: 90%;
-    //                     margin: 20px 5% 0 5%;
-    //                 }
-    //                 </style>
-    //             `;  
-
-    //         });
-    //     })
-    //     .catch( error => {
-    //         alert(error);
-    //         console.error('error:', error);
-    //     });
-        
-    // }
-    // else {
-        
-    // }
 });
