c58c1e20486034cc9f208b7eb350e0c94483452a
Nyarlat <nyarlat7@gmail.com>
2023-11-25 00:49:08 +0500

    Исправил пути к моделям

--- flaskapp/ml/inference.py
+++ flaskapp/ml/inference.py
@@ -2,22 +2,21 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import json
 
-model_path_group = "flaskapp/ml/models/fine-tune-rubert-26cls_512tokens/"
-model_path_themes = "flaskapp/ml/models/rubert_195cls_512tk_10e/"
+model_path_group = "models\\fine-tune-rubert-26cls_512tokens\\"
+model_path_themes = "models\\rubert_195cls_512tk_10e\\"
 
 group_tokenizer = AutoTokenizer.from_pretrained(model_path_group)
 group_model = AutoModelForSequenceClassification.from_pretrained(model_path_group)
-
 
 themes_tokenizer = AutoTokenizer.from_pretrained(model_path_themes)
 themes_model = AutoModelForSequenceClassification.from_pretrained(model_path_themes)
 
 
 def predict_group(text: str):
-    with open('flaskapp/ml/helpers/label2id_group.json', 'r') as json_file:
+    with open('helpers\\label2id_group.json', 'r') as json_file:
         label2id = json.load(json_file)
 
-    with open('flaskapp/ml/helpers/id2label_group.json', 'r') as json_file:
+    with open('helpers\\id2label_group.json', 'r') as json_file:
         id2label = json.load(json_file)
 
     tokens = group_tokenizer(text, return_tensors="pt")
@@ -51,3 +50,6 @@ def predict_theme(text: str, predicted_group: str):
     predicted_class_idx = torch.argmax(probs[0]).item()
 
     return id2label[str(predicted_class_idx)]
+
+
+predict_group("Почему полк не будет стоять на митинге,а пойдет в парк Гагарина. Кто это придумал?")
